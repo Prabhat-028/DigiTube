819a9e2563223ec9d10eac5386ad8624b354b3e6
Prabhat020 <kumar.prabhat23343@jeckukas.org.in>
2026-02-20 18:57:45 +0530

    created the methods for generating jwt token and hash password

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { UNSAFE_createClientRoutesWithHMRRevalidationOptOut } from "react-router-dom";
 
 const userSchema = new Schema({
     username: {
@@ -43,4 +46,45 @@ const userSchema = new Schema({
 }, {
 	timestamps:true
 });
+
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) return next();
+	this.password = await bcrypt.hash(this.password, 12);
+	next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+	return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+
+	return jwt.sign({
+		//this field is for the information which we encode 
+		_id:this._id,
+		fullName:this.fullName,
+		email:this.email,
+		username:this.username,
+	}, 
+		//this field is for the SECRET_KEY
+		process.env.ACCESS_TOKEN_SECRET, {
+			//this field is for expiry 
+			expiresIn:ACCESS_TOKEN_EXPIRY
+		}
+	)
+
+ }
+
+userSchema.methods.generateRefreshToken = function () {
+	return  jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+}
+
 export const User = mongoose.model("User", userSchema);
